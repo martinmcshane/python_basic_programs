b94598617df9de3e7d68ee383f3305c55778e9cb
martinmcshane <martinmcshane@gmail.com>
2022-11-11 17:27:55 +0000

    messing about

---
+++ test.py
@@ -0,0 +1,24 @@
+def listArray():
+    names = ["mac","john","dave"]
+    return names
+
+def printNames():
+    dickhead = listArray()
+    print(dickhead[0])
+
+'''
+def getNumber():
+    number = int(input("enter number"))
+    return number
+
+def printNumber():
+    dave = getNumber()
+    print(dave)
+
+printNumber()'''
+printNames()
+
+def someMaths(a,b):
+    return (a+b)
+
+print(someMaths(1000,3))
